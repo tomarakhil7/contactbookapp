b485cc6b20e96b6725cd19bf026cca81892d67fa
akhilt <akhilt@zeta.tech>
2019-03-13 16:58:42 +0530

    fixed

--- src/main/java/com/sample/Main.java
+++ src/main/java/com/sample/Main.java
@@ -4,8 +4,10 @@ import com.google.inject.Guice;
 import com.google.inject.Injector;
 
 public class Main {
-  Injector injector = Guice.createInjector(new BasicModule());
-  ContactBookService contactBookService =injector.getInstance(ContactBookService.class);
 
+  public static void main(String[] args) {
+    Injector injector = Guice.createInjector(new BasicModule());
+    ContactBookService contactBookService =injector.getInstance(ContactBookService.class);
+  }
 
 }
